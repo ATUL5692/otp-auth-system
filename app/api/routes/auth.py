f4958329f0be1c49b98dc7e9d0acbf15954fd001
ATUL5692 <aksinha1998@gmail.com>
2026-04-09 15:22:40 +0530

    feat: add user creation and persistent login

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.api.deps import get_db
 from app.services.otp_service import create_otp, verify_otp
+from app.core.security import create_access_token
+from app.services.user_service import get_user_by_phone, create_user
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
@@ -20,7 +22,23 @@ def send_otp(phone_number: str, db: Session = Depends(get_db)):
 def verify(phone_number: str, otp: str, db: Session = Depends(get_db)):
     success, message = verify_otp(db, phone_number, otp)
 
+    if not success:
+        return {
+            "success": False,
+            "message": message
+        }
+
+    # 🔥 user logic
+    user = get_user_by_phone(db, phone_number)
+
+    if not user:
+        user = create_user(db, phone_number)
+
+    token = create_access_token({"sub": str(user.id)})
+
     return {
-        "success": success,
-        "message": message
+        "success": True,
+        "message": "Login successful",
+        "access_token": token,
+        "token_type": "bearer"
     }
